b8059cd4419a2106d7a7c581b4fda5f7cb85a646
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-05-02 11:37:11 -0700

    "Completed endpoints -stretch goals"

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,2 +1,25 @@
 class BotsController < ApplicationController
+
+    def index
+        bots = Bot.all
+        render json: bots
+    end
+
+    def create
+        # Create a new bot
+        bot = Bot.create(bot_params)
+        render json: bot
+    end
+
+    private
+    def bot_params
+        params.require(:bot).permit(:name, :age, :weight, :enjoys, :image)
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 end
--- spec/requests/bots_spec.rb
+++ spec/requests/bots_spec.rb
@@ -2,6 +2,49 @@ require 'rails_helper'
 
 RSpec.describe "Bots", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "it gets a list of bots" do
+      Bot.create(
+        name: 'KillaMikeX',
+        age: 3,
+        weight: 157,
+        enjoys: 'feasting on the hydrolic fluid of my enemies.',
+        image: 'killamikex.jpeg'
+      )
+
+      # Make a request
+      get '/bots'
+
+      bot = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(bot.length).to eq 1
+    end  
+  end
+
+  describe "POST /create" do
+    it "creates a bot"do
+      # The Params we are going to send in this request
+      bot_params = {
+        bot: {
+          name: 'KillaMikeX',
+          age: 3,
+          weight: 157,
+          enjoys: 'feasting on the hydrolic fluid of my enemies.',
+          image: 'killamikex.jpeg'
+        }
+      }
+
+      # Send request to the server
+      post '/bots', params: bot_params
+
+      # Assure that we got a success back
+      expect(response).to have_http_status(200)
+
+      # Look up the bot we created in the db
+      bot = Bot.first
+
+      # Assure that the created bot has the correct attributes
+      expect(bot.name).to ep 'KillaMikeX'
+
+    end
   end
 end
